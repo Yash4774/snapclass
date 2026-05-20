2c7b01dcdb11ad0d029dad57707b59bf915e863f
Sahil Srivastava <k.sahil4774@gmail.com>
2026-05-20 20:33:00 +0530

    final deployment fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath("./src"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from home_screen import home_screen
 from teacher_screen import teacher_screen
