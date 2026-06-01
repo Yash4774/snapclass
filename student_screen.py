51edb45d6805903edbe49297a0b431af2533c8fd
Sahil Srivastava <k.sahil4774@gmail.com>
2026-06-01 14:43:34 +0530

    Added voice enrollment registration flow

--- student_screen.py
+++ student_screen.py
@@ -219,6 +219,7 @@ def student_screen():
                         st.session_state.is_logged_in = True
                         st.session_state.user_role = 'student'
                         join_code = st.query_params.get("join-code")
+                        show_registration=True
 
 
                         st.session_state.student_data = student
